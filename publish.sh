c104d7c4d5b39c63869be109a250db78f31a022e
John McKenzie <jmckind@gmail.com>
2018-06-07 10:32:16 -0700

    Remove submodule and add publish script

---
+++ publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo -t minimo
+
+# Add generated content
+cd public
+git add .
+
+# Commit changes
+msg="Site published on `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push changes
+git push origin master
+
+# Return to project root
+cd ..
